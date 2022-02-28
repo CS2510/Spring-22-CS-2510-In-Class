92dd2304a90bdcc611c1bfec834445500fa4d4cf
B. Ricks <bricks@unomaha.edu>
2022-02-28 15:05:09 -0600

    Add end of Feb 28

--- 22.02.28-Ender/zen_game/RectangleGameObject.js
+++ 22.02.28-Ender/zen_game/RectangleGameObject.js
@@ -6,7 +6,10 @@ import RectangleDraw from "../engine/RectangleDraw.js";
 class RectangleGameObject extends GameObject{
   constructor(x,y,w,h,r,g,b,percent){
     super();
+    this.components.push(new Rectangle(this, x,y,w,h));
+    this.components.push(new RectangleDraw(this, "blue", "orange"));
     this.components.push(new RectangleUpdateComponent(this,x,y,w,h,r,g,b,percent));
+    
   }
   
 }
