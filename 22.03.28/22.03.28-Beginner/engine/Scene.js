c1ac1b193cb34848759a2605d86b2ff0a223a5b8
B. Ricks <bricks@unomaha.edu>
2022-03-23 11:18:55 -0500

    Add a text-based game.

---
+++ 22.03.28/22.03.28-Beginner/engine/Scene.js
@@ -0,0 +1,28 @@
+class Scene{
+  constructor(title, fillColor="black"){
+    this.title = title;
+    this.fillColor = fillColor;
+  }
+  restart(){
+    this.gameObjects = [];
+    this.start();
+  }
+  update(ctx){
+    for (let gameObject of this.gameObjects) {
+      gameObject.update(ctx);
+  }
+  }
+  draw(ctx){
+    ctx.fillStyle = this.fillColor;
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    for (let gameObject of this.gameObjects) {
+        gameObject.draw(ctx);
+    }
+  }
+  remove(){
+    this.gameObjects = this.gameObjects.filter(g=>!g.markForDelete);
+  }
+}
+
+export default Scene;
