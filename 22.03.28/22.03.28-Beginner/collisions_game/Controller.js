c41227ac1eeca4582988cc02cc36748cdd0129d3
B. Ricks <bricks@unomaha.edu>
2022-03-30 10:08:27 -0500

    Add other update components

--- 22.03.28/22.03.28-Beginner/collisions_game/Controller.js
+++ 22.03.28/22.03.28-Beginner/collisions_game/Controller.js
@@ -10,41 +10,82 @@ class Collider extends Component {
     this.collisionCircleGameObject = Game.findByNameOne("Circle");
     this.collisionRectangleGameObject = Game.findByNameOne("Rectangle");
     this.circleDebugLineGameObject = Game.findByNameOne("CircleDebugLine");
-    this.collisionPointGameObject = Game.findByTypeOne("CollisionPointGameObject");
+    
+    this.collisionPointGameObject = Game.findByNameOne("DynamicPoint");
 
     this.collisionCircle = this.collisionCircleGameObject.getComponent("Circle");
     this.collisionRectangle = this.collisionRectangleGameObject.getComponent("Rectangle");
     this.circleDebugLine = this.circleDebugLineGameObject.getComponent("Line");
+    
     this.collisionPoint = this.collisionPointGameObject.getComponent("Point");
 
     this.collisionCircleDraw = this.collisionCircleGameObject.getComponent("CircleDraw");
     this.collisionRectangleDraw = this.collisionRectangleGameObject.getComponent("RectangleDraw");
+    this.state = "Point";
   }
   update() {
-    
-    this.circleDebugLine.x = this.collisionCircle.x;
-    this.circleDebugLine.y = this.collisionCircle.y;
-    this.circleDebugLine.x2 = this.collisionPoint.x;
-    this.circleDebugLine.y2 = this.collisionPoint.y;
-
-    if(Collisions.inCollision(this.collisionCircle, this.collisionPoint)){
-      this.collisionCircleDraw.strokeStyle = "yellow";
+    if(Input.getKeyDown("1")){
+      console.log("Changing to 1")
+      this.state = "Point"
     }
-    else{
-      this.collisionCircleDraw.strokeStyle = "red";
+    if(Input.getKeyDown("2")){
+      console.log("Changing to 2")
+      this.state = "Circle"
     }
-
-    if(Collisions.inCollision(this.collisionRectangle, this.collisionPoint)){
-      this.collisionRectangleDraw.strokeStyle = "yellow";
-    }
-    else{
-      this.collisionRectangleDraw.strokeStyle = "red";
+    if(Input.getKeyDown("3")){
+      console.log("Changing to 3")
+      this.state = "Rectangle"
     }
 
 
 
-    
-    
+
+    if (this.state == "Point") {
+      this.circleDebugLine.x = this.collisionCircle.x;
+      this.circleDebugLine.y = this.collisionCircle.y;
+      this.circleDebugLine.x2 = this.collisionPoint.x;
+      this.circleDebugLine.y2 = this.collisionPoint.y;
+
+      if (Collisions.inCollision(this.collisionCircle, this.collisionPoint)) {
+        this.collisionCircleDraw.strokeStyle = "yellow";
+      }
+      else {
+        this.collisionCircleDraw.strokeStyle = "red";
+      }
+
+      if (Collisions.inCollision(this.collisionRectangle, this.collisionPoint)) {
+        this.collisionRectangleDraw.strokeStyle = "yellow";
+      }
+      else {
+        this.collisionRectangleDraw.strokeStyle = "red";
+      }
+    }
+    if (this.state == "Circle") {
+      this.circleDebugLine.x = this.collisionCircle.x;
+      this.circleDebugLine.y = this.collisionCircle.y;
+      this.circleDebugLine.x2 = this.collisionPoint.x;
+      this.circleDebugLine.y2 = this.collisionPoint.y;
+
+      if (Collisions.inCollision(this.collisionCircle, this.collisionPoint)) {
+        this.collisionCircleDraw.strokeStyle = "yellow";
+      }
+      else {
+        this.collisionCircleDraw.strokeStyle = "red";
+      }
+
+      if (Collisions.inCollision(this.collisionRectangle, this.collisionPoint)) {
+        this.collisionRectangleDraw.strokeStyle = "yellow";
+      }
+      else {
+        this.collisionRectangleDraw.strokeStyle = "red";
+      }
+    }
+
+
+
+
+
+
   }
 }
 
--- 22.03.28/22.03.28-Beginner/collisions_game/MainScene.js
+++ 22.03.28/22.03.28-Beginner/collisions_game/MainScene.js
@@ -7,25 +7,48 @@ import PrefabTextSmall from "../engine/PrefabTextSmall.js";
 import PrefabEmpty from "../engine/PrefabEmpty.js";
 import Controller from "./Controller.js"
 
-import CollisionPointGameObject from "./CollisionPointGameObject.js";
+import Point from "../engine/Point.js";
+import PointDraw from "../engine/PointDraw.js";
+
+import PointUpdateComponent from "./PointUpdateComponent.js";
+import CircleUpdateComponent from "./CircleUpdateComponent.js";
+import RectangleUpdateComponent from "./RectangleUpdateComponent.js";
 
 class MainScene extends Scene {
   constructor() {
     super("Main Pong Scene");
   }
 
-  start(){
+  start() {
 
-    this.gameObjects.push(new PrefabCircle("Circle", 150,150, 50));
-    this.gameObjects.push(new PrefabRectangle("Rectangle", 250,250, 50, 25));
-    this.gameObjects.push(new PrefabLine("CircleDebugLine", 0,0, 50, 25));
-    this.gameObjects.push(new PrefabTextLarge("LargeText", 20, 50, "Collision Game"));
-    this.gameObjects.push(new PrefabTextSmall("SmallText", 20, 450, "Move the mouse to test collisions."));
+    this.gameObjects.push(new PrefabCircle("Circle", 150, 150, 50));
+    this.gameObjects.push(new PrefabRectangle("Rectangle", 250, 250, 50, 25));
+    this.gameObjects.push(new PrefabLine("CircleDebugLine", 0, 0, 50, 25));
 
-    this.gameObjects.push(new CollisionPointGameObject(0,0));
+    //Add the mouse-controlled point
+    this.gameObjects.push(
+      new PrefabEmpty("DynamicPoint")
+        .addComponent(new Point())
+        .addComponent(new PointDraw(null, "transparent", "magenta"))
+        .addComponent(new PointUpdateComponent()));
+
+    //Add the mouse-controlled Circle
+    this.gameObjects.push(
+      new PrefabCircle("DynamicCircle")
+        .addComponent(new CircleUpdateComponent()));
+
+    //Add the mouse-controlled Rectangle
+    this.gameObjects.push(
+      new PrefabRectangle("DynamicRectangle")
+        .addComponent(new RectangleUpdateComponent()));
 
     this.gameObjects.push(new PrefabEmpty("Controller").addComponent(new Controller()));
-    
+
+    this.gameObjects.push(new PrefabTextLarge("LargeText", 20, 50, "Collision Game"));
+    this.gameObjects.push(new PrefabTextSmall("SmallText", 20, 450, "Move the mouse to test collisions."));
+    this.gameObjects.push(new PrefabTextSmall("Instructions", 20, 500, "Type 1 to switch to a point"));
+    this.gameObjects.push(new PrefabTextSmall("Instructions", 20, 550, "Type 2 to switch to a circle"));
+    this.gameObjects.push(new PrefabTextSmall("Instructions", 20, 600, "Type 3 to switch to a rectangle"));
   }
 }
 
