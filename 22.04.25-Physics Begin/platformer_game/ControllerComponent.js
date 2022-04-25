ce55491d0aba9122f94836c4cf6fee1df6e2bb54
B. Ricks <bricks@unomaha.edu>
2022-04-25 11:29:14 -0500

    Fix time-dependent speeds

--- 22.04.25-Physics Begin/platformer_game/ControllerComponent.js
+++ 22.04.25-Physics Begin/platformer_game/ControllerComponent.js
@@ -4,33 +4,25 @@ import Input from "../engine/Input.js"
 import Collisions from "../engine/Collisions.js";
 
 import Time from "../engine/Time.js"
-import Rectangle from "../engine/components/Rectangle.js";
 
 
 class ControllerComponent extends Component {
   constructor(parent) {
     super(parent);
+
+    //Constants
+    this.lateralAcceleration = 32;
+    this.gravity = 32;
+    this.jumpForce = 50;
+
+    //Variables
     this.velocityY = 0;
     this.velocityX = 0;
     this.jumpTimer = 0;
-    this.jumpStep = 1;
-    this.canJump = false;
-    this.forceMultiplier = 2;
-    this.lateralAcceleration = 32;
-    this.gravity = 32 * this.forceMultiplier;
-    this.jumpForce = 32 * this.forceMultiplier;
-    this.FALLING = 0;
-    this.RESTING = 1;
-    this.JUMPING = 2
-    this.state = this.FALLING;
-    this.restingIndex = -1;
     this.gameTimer = 0;
-
-
-
   }
   update(ctx) {
-    
+
     //Update the global timer 
     this.gameTimer += Time.secondsBetweenFrame;
 
@@ -48,9 +40,6 @@ class ControllerComponent extends Component {
     let newVelocityY = this.velocityY;
     let newVelocityX = this.velocityX;
 
-    console.log(player.y + ", " + newVelocityY);
-
-    
 
     //Track instantaneous forces
     let frameAccelerationY = 0;
@@ -61,40 +50,40 @@ class ControllerComponent extends Component {
 
     //Track jumping force
     let frameJumpForce = 0;
-    frameGravity = this.gravity ;
+    frameGravity = this.gravity;
 
-    //this.jumpTimer += Time.secondsBetweenFrame;
+    this.jumpTimer += Time.secondsBetweenFrame;
     frameAccelerationY = frameGravity
 
     if (Input.getKeyDown(" ") || Input.getKeyDown("ArrowUp")) {
-      frameJumpForce = -this.jumpForce  / Time.secondsBetweenFrame;
-      this.jumpTimer = 0;
+      if (this.jumpTimer >= 1) {
+        if (this.velocityY == 0) {
+          //Division to make it frame-rate independent
+          frameJumpForce = -this.jumpForce / Time.secondsBetweenFrame;
+          this.jumpTimer = 0;
+        }
+      }
     }
 
     //Deal with left and right movement
-
+    //Division to make it frame-rate independent
     if (Input.getKey("ArrowLeft"))
-      frameAccelerationX = -this.lateralAcceleration / Time.secondsBetweenFrame ;
+      frameAccelerationX = -this.lateralAcceleration / Time.secondsBetweenFrame;
     else if (Input.getKey("ArrowRight"))
-      frameAccelerationX = this.lateralAcceleration  / Time.secondsBetweenFrame;
+      frameAccelerationX = this.lateralAcceleration / Time.secondsBetweenFrame;
     else {
       frameAccelerationX = 0;
     }
 
+    //Get all the bricks
     let brickGameObjects = Game.findByName("PrefabBrick");
-    this.canJump = false;
-
+    
     if (frameJumpForce != 0) {
       frameAccelerationY = frameJumpForce;
-      this.state = this.JUMPING;
     }
     else {
-      if (this.state == this.FALLING) {
-        frameAccelerationY = frameGravity;
-      }
-      else if (this.state == this.RESTING) {
-        frameAccelerationY = 0;
-      }
+         frameAccelerationY = frameGravity;
+      
     }
 
     //Update velocity based on acceleration
@@ -106,13 +95,11 @@ class ControllerComponent extends Component {
     newX += newVelocityX * Time.secondsBetweenFrame;
 
 
+    //Assign the new position values
     player.x = newX;
     player.y = newY;
 
-    //if(player.y <= -10)
-    //  console.log(50);
-
-
+    //Assign the new velocity values
 
     this.velocityX = newVelocityX;
     this.velocityY = newVelocityY;
@@ -120,41 +107,44 @@ class ControllerComponent extends Component {
 
     var cont = true;
     while (cont) {
+      //Get a list of the rectangles that are in collision with the player
       let collisions = brickGameObjects.filter(b => Collisions.inCollision(player, b.getComponent("Rectangle"))).map(b => b.getComponent("Rectangle"));
-      
+
+      //Stop if there are no collisions
       if (collisions.length == 0) break;
 
+      //Get overlap values
       let maxBelow = this.belowMax(collisions, player);
       let maxAbove = this.aboveMax(collisions, player);
       let maxRight = this.rightMax(collisions, player);
       let maxLeft = this.leftMax(collisions, player);
 
       let max = Math.max(maxBelow, maxAbove, maxRight, maxLeft)
-      //console.log(max);
-      if(max == maxBelow){
+
+      if (max == maxBelow) {
         player.y -= max;
         this.velocityY = 0;
         newVelocityY = 0;
       }
-      else if(max == maxRight){
+      else if (max == maxRight) {
         player.x -= max;
         this.velocityX = 0;
         newVelocityX = 0;
         ;
       }
-      else if(max == maxLeft){
+      else if (max == maxLeft) {
         player.x += max;
         this.velocityX = 0;
         newVelocityX = 0;
       }
-      else if(max == maxAbove){
+      else if (max == maxAbove) {
         player.y += max;
         this.velocityY = 0;
         newVelocityY = 0;
       }
       break;
     }
-    
+
 
     //Now update the text
     //Y physics game objects
@@ -195,7 +185,7 @@ class ControllerComponent extends Component {
 
 
   }
-  belowMax(collisions, player){
+  belowMax(collisions, player) {
     let belowCollisions = collisions.filter(c => Collisions.isCollidingDown(player, c));
     if (belowCollisions.length > 0) {
       //console.log("Below Collision Resolution");
@@ -204,7 +194,7 @@ class ControllerComponent extends Component {
     }
     return -1;
   }
-  aboveMax(collisions, player){
+  aboveMax(collisions, player) {
     let aboveCollisions = collisions.filter(c => Collisions.isCollidingUp(player, c));
     if (aboveCollisions.length > 0) {
       //console.log("Above Collision Resolution");
@@ -213,7 +203,7 @@ class ControllerComponent extends Component {
     }
     return -1;
   }
-  rightMax(collisions, player){
+  rightMax(collisions, player) {
     let rightCollisions = collisions.filter(c => Collisions.isCollidingRight(player, c));
     if (rightCollisions.length > 0) {
       //console.log("Right Collision Resolution");
@@ -222,7 +212,7 @@ class ControllerComponent extends Component {
     }
     return -1;
   }
-  leftMax(collisions, player){
+  leftMax(collisions, player) {
     let leftCollisions = collisions.filter(c => Collisions.isCollidingLeft(player, c));
     if (leftCollisions.length > 0) {
       //console.log("Left Collision Resolution");
