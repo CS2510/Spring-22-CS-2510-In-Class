82c9d5ab7b058e21e984107103cfe555be9b9a40
B. Ricks <bricks@unomaha.edu>
2022-04-25 15:18:56 -0500

    Update the Flappy Circle Game

--- 22.04.25-Physics Begin/flappy_game/ControllerComponent.js
+++ 22.04.25-Physics Begin/flappy_game/ControllerComponent.js
@@ -1,6 +1,7 @@
 import Component from "../engine/Component.js"
 import Game from "../engine/Game.js"
 import Input from "../engine/Input.js"
+import Collisions from "../engine/Collisions.js";
 
 import Time from "../engine/Time.js"
 
@@ -11,59 +12,56 @@ class ControllerComponent extends Component {
     this.velocity = 0;
     this.jumpTimer = 0;
     this.jumpStep = 1;
+    this.gravity = 32;
+    this.jumpForce = 50;
 
 
 
   }
   update(ctx) {
-
-    //Get the main character's game object
     let flappyGameObject = Game.findByNameOne("Flappy");
-
-    //Get the circle component of the main character 
-    //so we can move the character around
     let flappy = flappyGameObject.getComponent("Circle");
 
     let frameAcceleration = 0;
 
-    //Possible: Add jump timer
-    //Respond to input
-    //Respond to gravity
+    frameAcceleration = this.gravity;
+
+    this.jumpTimer += Time.secondsBetweenFrame;
+    if (this.jumpTimer >= this.jumpStep) {
+
+      if (Input.getKeyDown(" ")) {
+        frameAcceleration = -this.jumpForce / Time.secondsBetweenFrame ;
+        this.jumpTimer = 0;
+      }
+    }
 
 
     //Update velocity based on acceleration
-    
-    //Cap velocity (terminal velocity)
-    
-    //Update position based on velocity
-    
+    this.velocity += frameAcceleration * Time.secondsBetweenFrame;
 
-    //Game end condition
+    //Set the terminal velocity
+    this.velocity = Math.min(100, this.velocity);
+
+    //Update position based on velocity
+    flappy.y += this.velocity * Time.secondsBetweenFrame;
+
+
     if (flappy.y >= 200) {
       Game.changeScene(0);
     }
 
     //Now update the text
-    //Get the text game objects
     let positionGameObject = Game.findByNameOne("PositionText");
     let velocityGameObject = Game.findByNameOne("VelocityText");
     let accelerationGameObject = Game.findByNameOne("AccelerationText");
 
-    //Get the text components
     let position = positionGameObject.getComponent("Text");
     let velocity = velocityGameObject.getComponent("Text");
     let acceleration = accelerationGameObject.getComponent("Text");
 
-    //Set the text component values
-    position.text = "Position: " + (0).toFixed(2);
-    velocity.text = "Velocity: " + (0).toFixed(2);
-    acceleration.text = "Acceleration: " + (0).toFixed(2);
-
-
-
-
-
-
+    position.text = "Position: " + flappy.y.toFixed(2);
+    velocity.text = "Velocity: " + this.velocity.toFixed(2);
+    acceleration.text = "Acceleration: " + frameAcceleration.toFixed(2);
   }
 
 }
